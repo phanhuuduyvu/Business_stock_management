300967a36d69df67837b97f4a5945e258f34f7f8
phanhuuduyvu <phanhuuduyvu2411@gmail.com>
2025-08-09 17:37:58 +0700

    Vu add new code

--- UI/Src/Javascript/home.js
+++ UI/Src/Javascript/home.js
@@ -18,3 +18,51 @@ new Chart(pieChart, {
         }]
     }
 })
+
+const baseOptions ={
+    reponsive: true,
+    //maintainAspectRatio: false,
+    plugins: {
+        legend: {display: true, position: 'bottom'},
+        tooltip: { enabled: true}
+    },
+    layout: {padding: 0}
+}
+
+const lineChart = document.getElementById('lineChart').getContext('2d');
+
+if (lineChart) {
+    new Chart(lineChart, {
+        type: 'line',
+        data: {
+            lables: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul'],
+            datasets: [
+                {
+                    label: 'Team A',
+                    data: [42, 7, 89, 16, 54, 3, 78],
+                    borderColor:'rgb(156, 93, 93)',
+                    backgroundColor: ' #5b88bd',
+                    fill: true,
+                    tension: 0.35,
+                    pointRadius: 3
+                },
+                {
+                    label: 'Team B',
+                    data: [25, 61, 8, 93, 47, 12, 36],
+                    borderColor:'rgba(11, 11, 8, 0.47)',
+                    backgroundColor: 'rgb(88, 231, 122)',
+                    fill: true,
+                    tension: 0.35,
+                    pointRadius: 3
+                },
+            ]
+        },
+        option: {
+            ...baseOptions,
+            scale: {
+                x: { grid: {display: flase}},
+                y: { beginAtZero: true }
+            }
+        }
+    })
+}
